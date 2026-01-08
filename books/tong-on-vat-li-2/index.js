ac6ac1c5ee74445df368cc8fbd32c82bf61a0812
deanveb <Annhi1975@gmail.com>
2026-01-08 15:34:59 +0700

    -Sort lessons by name -Removed extra line in index.html

--- books/tong-on-vat-li-2/index.js
+++ books/tong-on-vat-li-2/index.js
@@ -2,11 +2,27 @@ async function displayContent() {
   const list = document.getElementById('lessons-list')
   const response = await fetch("data/ds-bai-hoc/lessons.json");
   const data = await response.json();
-  console.log(data);
+  // TODO: improve the sorting algorithm to be sorted according to lessons order
+  data.sort((a, b) => {
+    // return b.id - a.id;
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) {
+        return -1; // nameA comes first
+    }
+    if (nameA > nameB) {
+        return 1; // nameB comes first
+    }
+    return 0; // names are equal
+  });
+  // console.log(data);
 
   data.forEach(lesson => {
     const newLesson = document.createElement('li');
-    newLesson.class = 'lesson';
+    newLesson.className = 'lesson';
+    // this feel wrong
+    newLesson.id = lesson['name'];
     const link = document.createElement('a');
     link.href = `pages.html?name=${lesson['name']}`;
     link.innerHTML = lesson['name'];
@@ -15,4 +31,6 @@ async function displayContent() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {displayContent();})
+document.addEventListener('DOMContentLoaded', (e) => {
+  displayContent();
+})
